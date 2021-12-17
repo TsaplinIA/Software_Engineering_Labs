6952d4d38da6b3292b94488dad07175c9a9c8cfd
TsaplinIA <tsapli.ia@edu.spbstu.ru>
2021-12-17 03:44:09 +0300

    #4

--- converter.py
+++ converter.py
@@ -1,14 +1,39 @@
 symbols = "0123456789ABCDEF"
 
 
-def convert_from_decimla(N, base):    
+def convert_from_decimal(N, base):    
     div, mod = divmod(N, base)        
     return convert_from_decimal(div, base) + symbols[mod] if div else symbols[mod]
 
 
+def check_numeral_system(numeral_system):
+	if not numeral_system.isdigit(): return None
+	result = int(numeral_system)
+	if result in range(2, 17): # [2, 16]
+		return result
+	else:
+		return None
+
+
+def check_value(value, numeral_system):
+	s = symbols[:numeral_system]
+	for char in value:
+		if char not in s:
+			return False
+	return True
+
+
 def main(current_numeral_system, value, new_numeral_system):
-	current_numeral_system = int(current_numeral_system)
-	new_numeral_system = int(new_numeral_system)
+	current_numeral_system = check_numeral_system(current_numeral_system)
+	if current_numeral_system == None:
+		return "Wrong current numeral system"
+	
+	if not check_value(value, current_numeral_system):
+		return "Wrong value"
+	
+	new_numeral_system = check_numeral_system(new_numeral_system)
+	if new_numeral_system == None:
+		return "Wrong new numeral system"
 	
 	if current_numeral_system == 10:
 		return convert_from_decimal(int(value), new_numeral_system)
